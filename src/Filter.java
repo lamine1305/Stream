685c61f106c216b6578df2bc6ab03d1dd5fafcb6
Lamine MEHIDI <lamine1305@gmail.com>
2021-02-22 09:27:06 +0100

    test git

--- src/Filter.java
+++ src/Filter.java
@@ -7,7 +7,7 @@ public class Filter {
 		Integer[] tab = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 		List<Integer> list = Arrays.asList(tab);
 
-		// Filtrer les element de la liste
+		// Filtrer les elements de la liste
 		list.stream().filter(t -> t > 5).forEach(x -> System.out.print(x + " "));
 	}
 }
